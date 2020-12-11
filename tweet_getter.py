a577e167b02e01e499a518b94fb9616deee95ee1
Jerdan1980 <jeremytinana@gmail.com>
2020-12-11 18:48:31 -0500

    Remove useless code

--- tweet_getter.py
+++ tweet_getter.py
@@ -22,41 +22,36 @@ csvFile = open('tweets.csv', 'a', encoding="utf-8")
 #Use csv writer
 csvWriter = csv.writer(csvFile)
 
-#twitter will stop you after two searches, so manually change this accordingly.
-queries = ["coronavirus", "snowboarding", "the game awards"]
+#get tweets in search parameters
+#limit to 2k since it throws an error a little after 2k
+#change the word to do another search
+for tweet in tweepy.Cursor(api.search,
+												q = "coronavirus",
+												since = "2020-12-5",
+												until = "2020-12-11",
+												tweet_mode="extended",
+												lang = "en").items(2000):
 
-#cycle through queries
-for query in queries:
-	print(query)
-	#get tweets in search parameters
-	#limit to 2k since it throws an error a little after 2k
-	for tweet in tweepy.Cursor(api.search,
-														q = "coronavirus",
-														since = "2020-12-5",
-														until = "2020-12-11",
-														tweet_mode="extended",
-														lang = "en").items(2000):
+# Write the tweets with no commas, the likes and retweet count into the csv
+try:
+		fav_count = (tweet.retweeted_status.favorite_count)
+except:
+		fav_count = (tweet.favorite_count)
 
-		# Write the tweets with no commas, the likes and retweet count into the csv
-		try:
-				fav_count = (tweet.retweeted_status.favorite_count)
-		except:
-				fav_count = (tweet.favorite_count)
+#grab the full tweet
+try:
+	msg = tweet.retweeted_status.full_text
+except AttributeError:
+	msg = tweet.full_text
 
-		#grab the full tweet
-		try:
-			msg = tweet.retweeted_status.full_text
-		except AttributeError:
-			msg = tweet.full_text
+#remove commas and newlines
+msg = msg.replace(",", "")
+msg = msg.replace("\n", " ")
 
-		#remove commas and newlines
-		msg = msg.replace(",", "")
-		msg = msg.replace("\n", " ")
+#remove the "RT @whatever: "
+msg = re.sub("^RT\s@\w+: ", "", msg)
 
-		#remove the "RT @whatever: "
-		msg = re.sub("^RT\s@\w+: ", "", msg)
-
-		csvWriter.writerow([msg, tweet.retweet_count, fav_count])
+csvWriter.writerow([msg, tweet.retweet_count, fav_count])
 
 #print statement so you know it finished.
 print("Done!")
