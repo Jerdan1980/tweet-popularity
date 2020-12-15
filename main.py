706382cc1792a8ae3c11ce6fdbd18d0a68015e3b
jseidel00 <65245069+jseidel00@users.noreply.github.com>
2020-12-14 22:45:23 -0500

    Added n-gram function

--- main.py
+++ main.py
@@ -1,76 +1,74 @@
+from models import *
 import csv
 import random
-
-#used to create a dictionary
 import nltk
-from nltk.tokenize import word_tokenize
-import numpy as np
-import pickle #is needed by nltk... somehow
-from collections import Counter
-from nltk.stem import WordNetLemmatizer
 
-#import custom file
+# import custom file
 from models import *
 
-#create lists to hold data
+# create lists to hold data
 lexicon = []
 tweets = []
 retweets = []
 likes = []
 data = []
 
-#https://realpython.com/python-csv/
-#import csv
+# https://realpython.com/python-csv/
+# import csv
 with open('tweets.csv', encoding='utf-8') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
-	linecount = 0
-	for row in csv_reader:
-		#remove blank lines
-		if len(row) < 3:
-			continue
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    linecount = 0
+    for row in csv_reader:
+        # remove blank lines
+        if len(row) < 3:
+            continue
 
-		#save it
-		data.append(row)
+        # save it
+        data.append(row)
 
-		#add tweet's dictionary to lexicon
-		#it doesnt matter what order they get added so its fine here
-		lexicon += list(word_tokenize(row[0]))
-		
-		linecount += 1
-	print(f'Imported {linecount} tweets')
+        # add tweet's dictionary to lexicon
+        # it doesnt matter what order they get added so its fine here
+        lexicon += list(word_tokenize(row[0]))
+
+        linecount += 1
+    print(f'Imported {linecount} tweets')
 
 # remove dupes from lexicon
 lemmatizer = WordNetLemmatizer()
 lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
 print(f'Lexicon has {len(lexicon)} words')
 
-#shuffle data now so i dont have to do it later
+# shuffle data now so i dont have to do it later
 random.shuffle(data)
 
-#split the data up
+# split the data up
 for d in data:
-	tweets.append(d[0])
-	retweets.append(d[1])
-	likes.append(d[2])
-#delete data to make space
+    tweets.append(d[0])
+    retweets.append(d[1])
+    likes.append(d[2])
+# delete data to make space
 data = None
 print(f'Shuffled {len(tweets)} tweets')
 
-#sentdex dictionary
-sentdexdict = modifiedsentdex(lexicon)
-print(f'Sentdex dictionary has {len(sentdexdict)} words')
+# sentdex dictionary
+# sentdexdict = modifiedsentdex(lexicon)
+# print(f'Sentdex dictionary has {len(sentdexdict)} words')
 
-#simple dictionary
-featureset = createMatrix(tweets, sentdexdict)
-print("Finished simple dictionary model")
+# n-gram dictionary
+ngramdict = generateNGram(lexicon)
+print("Finished n-gram dictionary")
 
-#convert to numpy
-featureset = np.array(featureset)
-#divide data, 90% goes to training
-train_size = int(.9*len(featureset))
-endMarker = len(lexicon)-1
-#convert them all to double arrays so that it can do the thing.
-#otherwise they will throw a fit due to loss of information.
+# simple dictionary
+# featureset = createMatrix(tweets, lexicon)
+# print("Finished simple dictionary model")
+
+# convert to numpy
+featureset = np.array(ngramdict)
+# divide data, 90% goes to training
+train_size = int(.9 * len(featureset))
+endMarker = len(lexicon) - 1
+# convert them all to double arrays so that it can do the thing.
+# otherwise they will throw a fit due to loss of information.
 trainMatrix = np.array(featureset[:][:train_size], np.double)
 trainRetweets = np.array(retweets[:train_size], np.double)
 trainLikes = np.array(likes[:train_size], np.double)
@@ -81,7 +79,7 @@ print("Finished partitioning data")
 
 (rtWeights, likeWeights) = solve(trainMatrix, trainRetweets, trainLikes)
 
-#calculate the errors
+# calculate the errors
 (rtErr, likeErr) = loss(testMatrix, testRetweets, testLikes, rtWeights, likeWeights)
 print(f'Testing on {len(testRetweets)} tweets:')
 print(f'\tRetweet error: {rtErr}')
--- models.py
+++ models.py
@@ -1,74 +1,92 @@
 import numpy as np
 import nltk
+from nltk.lm import MLE
+from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk.tokenize import word_tokenize
 import numpy as np
-import pickle #is needed by nltk... somehow
+import pickle  # is needed by nltk... somehow
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
-from numba import jit #for SPEEDUP
+from numba import jit  # for SPEEDUP
 
-#the simple 1:1 dictionary we learned in class
+
+def generateNGram(lexicon):
+    n = 3
+    train_data, padded_sents = padded_everygram_pipeline(n, lexicon)
+    model = MLE(n)
+    model.fit(train_data, padded_sents)
+    featureset = []
+    for i in lexicon:
+        featureset.append(model.counts[i])
+
+    return featureset
+
+
+# the simple 1:1 dictionary we learned in class
 def createMatrix(tweets, lexicon):
-	lemmatizer = WordNetLemmatizer()
-	featureset = []
-	for t in tweets:
-		words = word_tokenize(t)
-		words = [lemmatizer.lemmatize(i) for i in words]
-		features = np.zeros(len(lexicon))
-		for w in words:
-			if w in lexicon:
-				features[lexicon.index(w)] += 1
-		#create augmented matrix of words and the retweets, since that seems to work better
-		featureset.append(list(features))
-	return featureset
+    lemmatizer = WordNetLemmatizer()
+    featureset = []
+    for t in tweets:
+        words = word_tokenize(t)
+        words = [lemmatizer.lemmatize(i) for i in words]
+        features = np.zeros(len(lexicon))
+        for w in words:
+            if w in lexicon:
+                features[lexicon.index(w)] += 1
+        # create augmented matrix of words and the retweets, since that seems to work better
+        featureset.append(list(features))
+    return featureset
 
-#a modified dectionary based off of sentdex's deeplearning tutorial
-#https://pythonprogramming.net/preprocessing-tensorflow-deep-learning-tutorial/
+
+# a modified dectionary based off of sentdex's deeplearning tutorial
+# https://pythonprogramming.net/preprocessing-tensorflow-deep-learning-tutorial/
 def modifiedsentdex(lexicon):
-	#creates a map of word to its count
-	w_counts = Counter(lexicon)
+    # creates a map of word to its count
+    w_counts = Counter(lexicon)
 
-	#make an array of just the counts
-	counts = []
-	for w in w_counts:
-		counts.append(w_counts[w])
-	
-	#find the upper and lower quartiles
-	lowerQuartile = np.percentile(counts, 25)
-	upperQuartile = np.percentile(counts, 90)
-	print(f'\tCutoffs: {lowerQuartile}, {upperQuartile}')
-	print(f'\tMax: {np.amax(counts)}')
+    # make an array of just the counts
+    counts = []
+    for w in w_counts:
+        counts.append(w_counts[w])
 
-	trunc_list = []
-	for w in w_counts:
-		#remove anything not in the middle 50%
-		if upperQuartile >= w_counts[w] >= lowerQuartile:
-			trunc_list.append(w)
-	return trunc_list
+    # find the upper and lower quartiles
+    lowerQuartile = np.percentile(counts, 25)
+    upperQuartile = np.percentile(counts, 90)
+    print(f'\tCutoffs: {lowerQuartile}, {upperQuartile}')
+    print(f'\tMax: {np.amax(counts)}')
 
-#@jit(nopython=True) #for SPEEDUP
+    trunc_list = []
+    for w in w_counts:
+        # remove anything not in the middle 50%
+        if upperQuartile >= w_counts[w] >= lowerQuartile:
+            trunc_list.append(w)
+    return trunc_list
+
+
+# @jit(nopython=True) #for SPEEDUP
 def solve(M, RTs, Likes):
-	#lstsq returns 4 outputs: x, resid, rank, and "singular values of a".
-	#we only want x, the first output.
-	#https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
-	#rcond both silences the futurewarning and sets cutoff values of the matrix
-	rtWeights = np.linalg.lstsq(M, RTs, rcond=None)[0]
-	likeWeights = np.linalg.lstsq(M, Likes, rcond=None)[0]
-	return (rtWeights, likeWeights)
+    # lstsq returns 4 outputs: x, resid, rank, and "singular values of a".
+    # we only want x, the first output.
+    # https://numpy.org/doc/stable/reference/generated/numpy.linalg.lstsq.html
+    # rcond both silences the futurewarning and sets cutoff values of the matrix
+    rtWeights = np.linalg.lstsq(M, RTs, rcond=None)[0]
+    likeWeights = np.linalg.lstsq(M, Likes, rcond=None)[0]
+    return (rtWeights, likeWeights)
 
-#@jit
+
+# @jit
 def loss(M, RTs, Likes, rtWeights, likeWeights):
-	#set initial counts to zero
-	rtErr = 0
-	likeErr = 0
+    # set initial counts to zero
+    rtErr = 0
+    likeErr = 0
 
-	#summ error squared
-	for i in range(0, len(RTs)):
-		rtErr += np.abs((M[:][i] @ rtWeights) - RTs[i])
-		likeErr += np.abs((M[:][i] @ likeWeights) - Likes[i])
-	
-	#divide by n
-	rtErr = rtErr / len(RTs)
-	likeErr = likeErr / len(Likes)
+    # summ error squared
+    for i in range(0, len(RTs)):
+        rtErr += np.abs((M[:][i] @ rtWeights) - RTs[i])
+        likeErr += np.abs((M[:][i] @ likeWeights) - Likes[i])
 
-	return (rtErr, likeErr)
+    # divide by n
+    rtErr = rtErr / len(RTs)
+    likeErr = likeErr / len(Likes)
+
+    return (rtErr, likeErr)
